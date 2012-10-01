3282ebe0933a4ea94092be6d05e23b7dd490dcf4
Florent Guilleux <florent2@gmail.com>
2012-10-01 16:14:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FloTestDeploy::Application.config.secret_token = '3fde9610dd1d8147e4014efca40e6014eca24649035081b92ba25066092c7cd349f503728c12473bd1c2e3eee2a3a31769d1d804d5e4c9fc2aa3b9efd9f53d01'
