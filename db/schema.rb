cc2e347b8e6ffe4b019e6fd67274f56c3083f1c7
Florent Guilleux <florent2@gmail.com>
2012-11-23 17:01:29 -0500

    sche

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121122155939) do
+ActiveRecord::Schema.define(:version => 20121123215855) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
